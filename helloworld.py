ab0cca8cba76185c7af2dc5b7d7a8fb07e488c4c
ahmedalibarhoom <ahmadbarhoomarg@gmail.com>
2022-09-19 18:33:33 -0300

    hellowold

---
+++ helloworld.py
@@ -0,0 +1,20 @@
+a=100
+b=200
+print(a + b)
+print(a * b)
+print(a / b)
+print(a - b)
+print(a % b)
+
+
+c = a + b
+print (c)
+
+a = a + b
+
+a += b
+a = a + b
+a = a * b
+a = a / b
+a = a % b
+a = a - b
