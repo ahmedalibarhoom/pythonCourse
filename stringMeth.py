0df53632245d614664a724e8162847eb06ed03ff
ahmedalibarhoom <ahmadbarhoomarg@gmail.com>
2022-09-23 16:16:39 -0300

    string

---
+++ stringMeth.py
@@ -0,0 +1,10 @@
+string = 'abcdeghik'
+
+print(string[0])
+print(string[0:8])
+print(string[0:8:2])
+print(string[0:8:3])
+
+print(string.find('h'))
+print(string.replace('h','o'))
+print(string.count('h'))
