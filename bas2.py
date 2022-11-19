a70c9ceb87d9dbe0734ac8e98ab05841ff4889ed
ahmedalibarhoom <ahmadbarhoomarg@gmail.com>
2022-11-19 13:24:25 -0300

    basics12

---
+++ bas2.py
@@ -0,0 +1,18 @@
+# num2 = 15.25
+#
+# print(type(num2))
+#
+# num1 = 100
+# print(id(num1))
+#
+# num1 = 102
+# print(id(num1))
+
+s = 'python'
+print(s)
+print(type(s))
+print(id(s))
+
+s = 'world'
+print(s)
+print(id(s))
