47d2374d05f52f50de8efed0113de88f9db58135
ahmedalibarhoom <ahmadbarhoomarg@gmail.com>
2022-12-06 23:45:32 -0300

    lesson2

---
+++ pyOperation1.py
@@ -0,0 +1,19 @@
+num1 = 10
+num2 = 20
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+
+print(10/3)
+print(10//2)
+print(10//3)
+
+print(10%3)
+
+print(5**3)
+
+num3 = 100
+num4 = 200
+print(num3 < num4)
+print(num3 > num4)
+print(num3 != num4)
