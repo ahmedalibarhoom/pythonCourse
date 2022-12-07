cc8908596ee6bf449c3ce91f832dee58b03d3554
ahmedalibarhoom <ahmadbarhoomarg@gmail.com>
2022-12-07 16:44:44 -0300

    pyopertation1

--- pyOperation1.py
+++ pyOperation1.py
@@ -1,19 +1,34 @@
-num1 = 10
-num2 = 20
-print(num1 + num2)
-print(num1 - num2)
-print(num1 * num2)
 
-print(10/3)
-print(10//2)
-print(10//3)
+# num1 = 10
+# num2 = 20
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+#
+# print(10/3)
+# print(10//2)
+# print(10//3)
+#
+# print(10%3)
+#
+# print(5**3)
+#
+# num3 = 100
+# num4 = 100
+# print(num3 < num4)
+# print(num3 > num4)
+# print(num3 != num4)
+#
+#
+# print(num3 == num4)
+#
+# print(num3 is num4)
 
-print(10%3)
-
-print(5**3)
-
-num3 = 100
-num4 = 200
-print(num3 < num4)
-print(num3 > num4)
-print(num3 != num4)
+l = [10,20,50]
+l2 = [10,20,50]
+print(l == l2)
+print(l is l2)
+print(type(l))
+print(id(l))
+print(id(l2))
+print(l is not l2)
