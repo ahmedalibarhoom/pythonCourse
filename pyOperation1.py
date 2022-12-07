e4d6c526e95d7dbe6f73ee12110b3deea234a0e5
ahmedalibarhoom <ahmadbarhoomarg@gmail.com>
2022-12-07 18:51:50 -0300

    opertion1

--- pyOperation1.py
+++ pyOperation1.py
@@ -24,11 +24,40 @@
 #
 # print(num3 is num4)
 
-l = [10,20,50]
-l2 = [10,20,50]
-print(l == l2)
-print(l is l2)
-print(type(l))
-print(id(l))
-print(id(l2))
-print(l is not l2)
+# l = [10,20,50]
+# l2 = [10,20,50]
+# print(l == l2)
+# print(l is l2)
+# print(type(l))
+# print(id(l))
+# print(id(l2))
+# print(l is not l2)
+
+
+# num1 = 100
+# num1 = num1 + 5
+# print(num1)
+#
+# num1-=6
+# print(num1)
+
+# num1 = 5
+# num2 = 3
+# print(bin(num1), bin(num2))
+# print(num1 | num2)
+
+
+print(10==110 and 20==20)
+print(10==110 or 20==20)
+
+
+
+
+
+
+
+
+
+
+
+
