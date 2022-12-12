148a278415442f28bb12640bd2f1e58f5ce5c77b
ahmedalibarhoom <ahmadbarhoomarg@gmail.com>
2022-12-12 11:25:26 -0300

    prueba

---
+++ ifcon1.py
@@ -0,0 +1,13 @@
+# num1 = 100
+# num2 = 200
+# if num1 > num2:
+#     print('num1 greater')
+# elif num2 > num1:
+#     print('num2 greater')
+# else:
+#     print('num1 and num2 are same')
+
+if 0 :
+    print('state in if block')
+else:
+    print('state in else block')
