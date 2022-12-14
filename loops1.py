b6326ff79d59e7416d2b67853cff8089ace0ef55
ahmedalibarhoom <ahmadbarhoomarg@gmail.com>
2022-12-14 15:28:39 -0300

    ara11

--- loops1.py
+++ loops1.py
@@ -1,22 +1,27 @@
+#
+#
+# l = [10,20,50,60]
+# for value in l:
+#     print(value)
+#
+#
+#
+#
+# print('   /|')
+# print('  / |')
+# print(' /  |')
+# print('/___|')
+#
+#
+# character_name = 'tefy'
+# age = '30'
+# print('there was a boy called '+character_name)
+# print('he was '+age+' years old')
+# character_name = 'ali'
+# print('he liked his name '+character_name)
+# print(character_name+' has one brother')
 
 
-l = [10,20,50,60]
-for value in l:
-    print(value)
-
-
-
-
-print('   /|')
-print('  / |')
-print(' /  |')
-print('/___|')
-
-
-character_name = 'tefy'
-age = '30'
-print('there was a boy called '+character_name)
-print('he was '+age+' years old')
-character_name = 'ali'
-print('he liked his name '+character_name)
-print(character_name+' has one brotherrr')
+print('ahmed is here in \n the class')
+print('ahmed is here in \n the \'class')
+print('ahmed is here' ' the class')
