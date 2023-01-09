45a2ae23b65945f59019d30f83edc16666f24d21
ahmedalibarhoom <ahmadbarhoomarg@gmail.com>
2023-01-09 11:50:38 -0300

    getinput1

--- functions.py
+++ functions.py
@@ -1,18 +1,35 @@
-a = "hello"
-b = "world"
-c = a + " " +b
+# a = "hello"
+# b = "world"
+# c = a + " " +b
+#
+# print(c)
+# d = a * 3
+# print(d)
+#
+#
+# list = [100,200,300,400,500]
+# a = ['a','b']
+# print(a[0])
+# print(list[0])
+# print(list[3])
+#
+# list.append(600)
+# print(list[5])
+# print(list)
 
-print(c)
-d = a * 3
-print(d)
+
+charachter_name = 'hasan'
+age = '40'
+print('there was a boy called ' +charachter_name)
+print('he was ' +age+ ' years old ')
+print('he liked his name '+charachter_name)
+print('but was not happy about being '+age)
+print(charachter_name+' has two brothers')
+print('he also '+charachter_name+' called alioso')
 
 
-list = [100,200,300,400,500]
-a = ['a','b']
-print(a[0])
-print(list[0])
-print(list[3])
 
-list.append(600)
-print(list[5])
-print(list)
+
+
+
+
