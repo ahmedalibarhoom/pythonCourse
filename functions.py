026550e102c498bc78fe0d7b463144c7ae6f779e
ahmedalibarhoom <ahmadbarhoomarg@gmail.com>
2023-01-10 15:54:56 -0300

    stringgg

--- functions.py
+++ functions.py
@@ -18,14 +18,52 @@
 # print(list)
 
 
-charachter_name = 'hasan'
-age = '40'
-print('there was a boy called ' +charachter_name)
-print('he was ' +age+ ' years old ')
-print('he liked his name '+charachter_name)
-print('but was not happy about being '+age)
-print(charachter_name+' has two brothers')
-print('he also '+charachter_name+' called alioso')
+# charachter_name = 'hasan'
+# age = '40'
+# print('there was a boy called ' +charachter_name)
+# print('he was ' +age+ ' years old ')
+# print('he liked his name '+charachter_name)
+# print('but was not happy about being '+age)
+# print(charachter_name+' has two brothers')
+# print('he also '+charachter_name+' called alioso')
+
+# string for text any texts
+
+
+text = 'HAmada'
+print(text)
+print(text.lower())
+print(text.upper())
+print(text.isupper())
+print(text.islower())
+print(len(text))
+print(text.lower().islower())
+print(text[2])
+print(text.index('m'))
+print(text.index('da'))
+print(text.replace('da','ma'))
+print(text+' he is the worst guy here'+' thank you')
+print('ham\ndaloa')
+print('ahmed is here\nand then he went there')
+
+print('ahmed\t is here\nand then he\' went there')
+
+# t make 4 spaces ***
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
