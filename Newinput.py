d0a6002211235b0f35b564e79ef91c31a3db8138
ahmedalibarhoom <ahmadbarhoomarg@gmail.com>
2023-01-17 12:56:35 -0300

    calculaor

---
+++ Newinput.py
@@ -0,0 +1,17 @@
+# name = input('what is your name')
+# age = input('your age')
+# print('welcome '+ name+' and your age is '+ age)
+
+
+# num1 = input('enter the first no: ')
+# num2 = input('enter the second no: ')
+# result = int(num1)+int(num2)
+# print(result)
+
+
+# to solve decimal numbers
+
+num1 = input('enter the first no: ')
+num2 = input('enter the second no: ')
+result = float(num1)+float(num2)
+print(result)
