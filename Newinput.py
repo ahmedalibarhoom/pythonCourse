5e063c5804d41ede09e93b7281ea13ba8ab63938
ahmedalibarhoom <ahmadbarhoomarg@gmail.com>
2023-01-18 19:00:53 -0300

    lists11

--- Newinput.py
+++ Newinput.py
@@ -25,15 +25,42 @@
 # print('please keep it '+adjective)
 
 
-name = input('enter new name')
-prounun = input('enter new prnoun')
-adjective = input('enter new adjective')
+# name = input('enter new name')
+# prounun = input('enter new prnoun')
+# adjective = input('enter new adjective')
+#
+# print('while '+name+' was reading '+prounun+' book')
+# print(prounun+' mom told her to clean '+prounun+' room')
+# print(name+' was '+adjective+' guy and '+adjective)
+# print(name+' want to be football player but '+prounun+' dad did not want')
+# print(name+' was sad cuz '+prounun+' dad did not helped '+prounun)
 
-print('while '+name+' was reading '+prounun+' book')
-print(prounun+' mom told him to clean '+prounun+' room')
-print(name+' was '+adjective+' guy and '+adjective)
-print(name+' want to be football player but '+prounun+' dad did not want')
-print(name+' was sad cuz '+prounun+' dad did not helped '+prounun)
+
+#many types of info in one place
+
+#[] this squaers tells python that im list
+
+# friends = [1, 'ahmed ali', 2.2 , True, False]
+# print(friends)
+# print(friends[1])
+# print(friends[2])
+
+friends = ['saad', 'ahmed', 'hasan', 'hosni']
+
+print(friends)
+print(friends[-1])
+print(friends[-2])
+print(friends[1:3]) # here the programme say no add factor 3 hosni
+
+print(friends[0:2])
+# to start count from specif unit until the end
+print(friends[1:])
+friends[1] = 'coco' # to change value inside the list
+print(friends[1])
+print(friends)
+
+
+
 
 
 
